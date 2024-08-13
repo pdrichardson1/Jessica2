9577f2ea6e321b345ddfd217eced7e37d3d60535
Pierre Richardson <prichardson@icstars.org>
2024-08-12 20:14:14 -0500

    jessica2

---
+++ gradingsystem/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// int score = 85;
+// char grade;
+
+// if (score >= 90) {
+//     grade = 'A';
+// }
+// else if (score >= 80) {
+//     grade = 'B';
+// }
+// else if (score >= 70) {
+//     grade = 'C';
+// }
+// else if (score >= 60) {
+//     grade = 'D';
+// }
+// else {
+//     grade = 'F';
+// }
+// Console.WriteLine("Your grade is: " + grade);
+
+// int score = 85;
+// char grade;
+
+// switch (score)
+// {
+//     case >= 90 and <= 100:
+//         grade = 'A';
+//         break;
+//     case >= 80 and < 90:
+//         grade = 'B';
+//         break;
+//     case >= 70 and < 80:
+//         grade = 'C';
+//         break;
+//     case >= 60 and < 70:
+//         grade = 'D';
+//         break;
+//     case < 60:
+//         grade = 'F';
+//         break;
+//     default:
+//         grade = 'F'; 
+//         break;
+// }
+// Console.WriteLine("Your grade is: " + grade);
+
+
+git 
+ 
